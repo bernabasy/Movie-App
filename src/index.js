a2f6b7183a765bf2047778ae0294b2fb903b28c9
tobuya <thomasobuya95@yahoo.com>
2023-01-12 11:32:47 +0300

    Render homepage list items on page load

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './style.css';
 import logos from './images/logo.png';
+import displayMovies from './modules/homepage.js';
 
 const logoimg = document.querySelector('#logo');
 logoimg.src = logos;
+
+window.addEventListener('load', displayMovies);
