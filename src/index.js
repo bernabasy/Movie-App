b9b6a462d4c12f43d8b5ff4ac4f4ee281008f73a
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-01-14 01:12:09 +0300

    Add contact section

--- src/index.js
+++ src/index.js
@@ -6,3 +6,22 @@ const logoimg = document.querySelector('#logo');
 logoimg.src = logos;
 
 showPopups();
+
+const about = document.querySelector('#about');
+const home = document.querySelector('#homes');
+
+home.addEventListener('click', () => {
+  const main = document.querySelector('#main');
+  const contact = document.querySelector('#contacts');
+  contact.style.display = 'none';
+  main.style.display = 'grid';
+});
+
+about.addEventListener('click', () => {
+  const main = document.querySelector('#main');
+  const contact = document.querySelector('#contacts');
+  main.style.display = 'none';
+  contact.style.display = 'block';
+});
+
+window.addEventListener('load', home);
